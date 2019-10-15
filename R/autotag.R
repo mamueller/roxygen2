588e6a3c402135daf7dc7dda9dbfc5134e01afbd
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-10-15 12:16:01 +0200

    started the autotag roclet

---
+++ R/autotag.R
@@ -0,0 +1,43 @@
+namespace_roclet <- function() {
+  roclet("namespace")
+}
+#' @export
+roclet_preprocess.roclet_autotag <- function(x, blocks, base_path) {
+
+  #lines <- blocks_to_ns(blocks, env, import_only = TRUE)
+  #NAMESPACE <- file.path(base_path, "NAMESPACE")
+
+  #if (length(lines) == 0 && !made_by_roxygen(NAMESPACE)) {
+  #  return(x)
+  #}
+
+  #results <- c(made_by("#"), lines)
+  #write_if_different(NAMESPACE, results, check = TRUE)
+
+  invisible(x)
+}
+
+#' @export
+roclet_process.roclet_autotag <- function(x, blocks, env, base_path) {
+  blocks_to_ns(blocks, env)
+}
+
+#' @export
+roclet_output.roclet_autotag <- function(x, results, base_path, ...) {
+  #NAMESPACE <- file.path(base_path, "NAMESPACE")
+  #results <- c(made_by("#"), results)
+
+  ## Always check for roxygen2 header before overwriting NAMESPACE (#436),
+  ## even when running for the first time
+  #write_if_different(NAMESPACE, results, check = TRUE)
+
+  #NAMESPACE
+}
+
+#' @export
+roclet_clean.roclet_autotag <- function(x, base_path) {
+  #NAMESPACE <- file.path(base_path, "NAMESPACE")
+  #if (made_by_roxygen(NAMESPACE)) {
+  #  unlink(NAMESPACE)
+  #}
+}
