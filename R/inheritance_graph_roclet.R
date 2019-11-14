715e1dd71389ab0b409cc3c67dc7e9e390ff3034
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-11-14 14:15:02 +0100

    implemented the inheritance graph roclet

--- R/inheritance_graph_roclet.R
+++ R/inheritance_graph_roclet.R
@@ -7,19 +7,50 @@ inheritance_graph_roclet <- function() {
 
 #' @export
 roclet_process.roclet_inheritance_graph<- function(x, blocks, env, base_path) {
-  g3 <- igraph::graph( c("John", "Jim", "Jim", "Jill", "Jill", "John")) # named vertices
-  links<-data.frame(from ="John", to="Jim")
-  links<-rbind(links,data.frame(from ="Jim" , to="Jill"))
-  links<-rbind(links,data.frame(from ="Jill", to="John"))
+  cl_names<-methods::getClasses(env)
+  cl_reps<-lapply(
+    cl_names,
+    function(name){getClass(name,where=env)}
+  )
+  links<-Reduce(
+    rbind,
+    lapply(
+      cl_reps,
+      function(rep){
+        cl_name<-as.character(attr(rep,'className'))
+        Reduce(
+          rbind,
+          lapply(
+            names(attr(rep,'subclasses')),
+            function(sub_cl_name){
+              data.frame(from=cl_name,to=sub_cl_name)}
+            )
+        )
+      }
+    )
+  )
+  
+  nodes<-Reduce(
+    rbind,
+    lapply(
+      cl_names,
+      function(cl_name) data.frame(id=cl_name)
+    )
+  )
 
-  nodes<-data.frame(id="John")
-  nodes<-rbind(nodes,data.frame(id ="Jim" ))
-  nodes<-rbind(nodes,data.frame(id ="Jill"))
-  nodes<-rbind(nodes,data.frame(id ="Karl"))
+  #links<-data.frame(from ="John", to="Jim")
+  #links<-rbind(links,data.frame(from ="Jim" , to="Jill"))
+  #links<-rbind(links,data.frame(from ="Jill", to="John"))
+
+  #nodes<-data.frame(id="John")
+  #nodes<-rbind(nodes,data.frame(id ="Jim" ))
+  #nodes<-rbind(nodes,data.frame(id ="Jill"))
+  #nodes<-rbind(nodes,data.frame(id ="Karl"))
 
   #g2 <- igraph::graph_from_data_frame( d=links,vertices=nodes,directed=TRUE) 
   #graph_attr(g2, "layout") <- layout_with_sugiyama(g2)$layout
   #plot(g2)
+
   list(links=links,nodes=nodes) 
 }
 
@@ -31,15 +62,23 @@ roclet_output.roclet_inheritance_graph<- function(x, results,  base_path,...) {
   links<-results$links
   nodes<-results$nodes
   g2 <- igraph::graph_from_data_frame( d=links,vertices=nodes,directed=TRUE) 
-  graph_attr(g2, "layout") <- layout_with_sugiyama(g2)$layout
+  igraph::graph_attr(g2, "layout") <- layout_with_sugiyama(g2)$layout
   # When the edge list has vertex names, the number of nodes is not needed
-    plot(
-       g2 
-        ,vertex.shape='circle'
-        #,vertex.size=4
-        #,vertex.label=as.character(nodes$id)
-        ,alpha=0.1
-        ,edge.arrow.size=.4
-        ,edge.arrow.width=.8
-    )
+    dir_path<-file.path(base_path,'inst','doc')
+    if(!file.exists(dir_path)){
+      dir.create(path=dir_path,recursive=TRUE)
+    }
+    p<-file.path(dir_path,'InheretanceGraph.pdf')
+    browser()
+    pdf(p)
+      plot(
+         g2 
+          ,vertex.shape='circle'
+          #,vertex.size=4
+          #,vertex.label=as.character(nodes$id)
+          ,alpha=0.1
+          ,edge.arrow.size=.4
+          ,edge.arrow.width=.8
+      )
+  dev.off()
 }
---
+++ tests/R6Unit/InheritanceGraphTest.R
@@ -0,0 +1,46 @@
+#!/usr/bin/Rscript
+## vim:set ff=unix expandtab ts=2 sw=2:
+require(R6Unit)
+InheritanceGraphTests<-R6Class("InheritanceGraphTests",
+	inherit=InDirScriptTest,
+  public=list(
+    targetPkgName=""
+    ,
+    #------------------------
+    setUp=function(){
+      source('../../writeDescriptionFile.R')
+      requireNamespace("pkgload")
+      requireNamespace("debugHelpers")
+      pkgload::load_all('../../../../',export_all=FALSE)
+    }
+    
+    ,
+    #--------------------------------
+	  test.ClassGraph=function(){
+      targetPkgName<-"S4ClassInheritance"
+      #requireNamespace("R6Unit")
+      # copy the files 
+      resourceDirName<-file.path("..","..","test_resources","example_packages")
+      pkgDirOrg="pkgDirOrg"
+      R6Unit::cpDir(file.path(resourceDirName,targetPkgName),pkgDirOrg)
+      # if necessarry add a default DESCRIPTION file
+      if (!file.exists(file.path(pkgDirOrg,"DESCRIPTION"))){ 
+        writeDescriptionFile(Imports="methods",pkgName=targetPkgName,pkgDir=pkgDirOrg)
+      }
+      roxygenize(pkgDirOrg,c("inheritance_graph_roclet"))
+	  }
+
+
+
+
+
+  )
+)
+
+############################################ 
+if(is.null(sys.calls()[[sys.nframe()-1]])){
+  s=get_suite_from_file(get_Rscript_filename())
+  #s$parallel <- 1 
+  tr<-s$run()
+  tr$print_summary()
+}
