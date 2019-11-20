56c074b0fa5e22cd76140f07bfae11a03c91f6ff
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-11-20 14:42:53 +0100

    some changes in the graph'

--- R/inheritance_graph_roclet.R
+++ R/inheritance_graph_roclet.R
@@ -62,34 +62,38 @@ roclet_output.roclet_inheritance_graph<- function(x, results,  base_path,...) {
   links<-results$links
   nodes<-results$nodes
   g2 <- igraph::graph_from_data_frame( d=links,vertices=nodes,directed=TRUE) 
-  igraph::graph_attr(g2, "layout") <- layout_with_sugiyama(g2)$layout
-  # When the edge list has vertex names, the number of nodes is not needed
-    #dir_path<-file.path(base_path,'inst','doc')
-    dir_path<-file.path(base_path,'vignettes')
+  l<-layout_with_sugiyama(g2,hgap=.01)$layout
+  #swap columns
+  browser()
+  m<-l
+  old_y=l[,2]
+  old_x=l[,1]
+  m[,1]<-.2*(max(old_y)-old_y)+1
+  m[,2]<-old_x
+  igraph::graph_attr(g2, "layout") <- m
+  # igraph::graph_attr(g2, "layout") <- layout_on_grid(g2)
+
+    dir_path<-file.path(base_path,'inst','doc')
     if(!file.exists(dir_path)){
       dir.create(path=dir_path,recursive=TRUE)
+      sleep(1)
     }
-    graph_file_name<-'InheretanceGraph.pdf'
-    p<-file.path(dir_path,graph_file_name)
-    p2<-file.path(dir_path,'.install_extras')
-    if(!file.exists(p2)){
-      file.create(p2)
-      Sys.sleep(1)
-    }
-    l<-read_lines(p2)
-    l<-append(l,paste0('^',graph_file_name,'$'))
-    write_lines(l,p2)
-
-    #browser()
+    p<-file.path(dir_path,'InheretanceGraph.pdf')
+    vlfs=.2
     pdf(p)
       plot(
-         g2 
-          ,vertex.shape='circle'
-          #,vertex.size=4
-          #,vertex.label=as.character(nodes$id)
-          ,alpha=0.1
-          ,edge.arrow.size=.4
-          ,edge.arrow.width=.8
+         g2,
+         #vertex.shape='circle',
+         vertex.shape='rectangle',
+         vertex.label.font=2,
+         vertex.label.cex=vlfs,
+         vertex.angle=pi/2,
+         vertex.size=4*vlfs*stringr::str_length(nodes$id),
+         vertex.size2=14*vlfs,
+         vertex.label=as.character(nodes$id),
+         alpha=0.1,
+         edge.arrow.size=.1,
+         edge.arrow.width=.8
       )
   dev.off()
 }
